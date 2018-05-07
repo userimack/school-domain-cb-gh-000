c9e2516db54ae6f5e298736b23efe65048cb93da
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-07 17:40:01 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    #if roster.keys().include?(grade)
+    #  roster[grade] << name
+    #else
+    #  roster[grade] = [name]
+    #end
+
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    new_roster = {}
+    roster.each do |grade, name|
+      new_roster[grade] = name.sort
+    end
+    new_roster
+  end
+end
